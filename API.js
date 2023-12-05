5486b87147f43e9e3ba81dd8026bfc7fff69cb87
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2023-12-05 14:37:16 +0000

    Update database connection string and handle errors in API.js

--- API.js
+++ API.js
@@ -8,16 +8,34 @@ export class API{
         this.app = express() //app es express
         this.conectarBD()
         this.enrutarPeticiones()
+        this.manejarErrores()
     }
     despertarServidor(){
-        this.app.listen(3000,()=>console.log("Servidor encendido..."))
+        const PORT = process.env.PORT || 3000;
+        this.app.listen(PORT,()=>console.log(`Servidor encendido en el puerto ${PORT}...`))
     }
     enrutarPeticiones(){
+        this.app.get('/', (req, res) => {
+            res.send('¡Bienvenido a mi API!');
+        });
+    
         this.app.use(cors())
         this.app.use(express.json()) //habilitamos la recepcion de datos desde el body de la peticion
         this.app.use('/',rutas) //habilitamos las rutas o endpoints
     }
-    conectarBD(){
-        establecerConexion()
+    manejarErrores(){
+        this.app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send('¡Algo salió mal!');
+        });
+    }
+    async conectarBD(){
+        try {
+            await establecerConexion();
+            console.log('Conexión a la base de datos establecida con éxito');
+        } catch (error) {
+            console.error('No se pudo conectar a la base de datos', error);
+            process.exit(1);
+        }
     }
 }
